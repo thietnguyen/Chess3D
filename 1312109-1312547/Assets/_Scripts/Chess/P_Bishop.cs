1d45fd15a4f8a5f153cb5115be6118617e96c86e
thietnguyen <thietnguyen25071995@gmail.com>
2017-01-15 04:35:52 +0700

    Bishop Move and Attack

--- 1312109-1312547/Assets/_Scripts/Chess/P_Bishop.cs
+++ 1312109-1312547/Assets/_Scripts/Chess/P_Bishop.cs
@@ -6,6 +6,34 @@ public class P_Bishop : BaseChess {
 
     public override void BeSelected()
     {
+        int[] x_array = new int[4] { -1, -1, 1, 1};
+        int[] y_array = new int[4] { -1, 1, -1, 1};
+
+        for (int i = 0; i<4; i++)
+        {
+            CLocation c = this.Location;
+            while (true)
+            {
+                c = new CLocation(c.X + x_array[i], c.Y + y_array[i]);
+                if (Helper.checkLocation(c))
+                {
+                    Cell cell = ChessBoard.Current.Cell[c.X][c.Y];
+                    if (cell.CurrentChess != null)
+                    {
+                        if (cell.CurrentChess.Player != BaseGameCTL.Current.CurrentPlayer)
+                            _targetedCell.Add(cell);
+                        break;
+                    }
+                    _targetedCell.Add(cell);
+                }
+                else
+                    break;
+                
+            }
+        }
+        
+        foreach (var item in _targetedCell)
+            item.SetCellState(Ecellstate.TAGETED);
     }
 
 }
---
+++ 1312109-1312547/Assets/_Scripts/Helpers/Helper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helper {
+
+    public static bool checkLocation(CLocation c)
+    {
+        if (c.X >= 0 && c.X <= 7 && c.Y >= 0 && c.Y <= 7)
+            return true;
+        else
+            return false;
+    }
+}
